22117dade601849604c04dc89d2e9c0a0ae58cc3
DzejPi <dzejjpii@gmail.com>
2023-04-29 16:05:42 +0200

    Prepared the placeholder enemy script and node.

---
+++ scripts/EnemyScript.gd
@@ -0,0 +1,28 @@
+extends KinematicBody
+
+
+var health = 100
+
+var is_stalking_player = false
+var is_attacking_player = false
+var is_runnig_away_from_player = false
+
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func stalk_player():
+	pass
+	
+
+func attack_player():
+	pass
+
+
+func run_aray():
+	pass
--- scripts/GameSceneOne.gd
+++ scripts/GameSceneOne.gd
@@ -1,1 +1,3 @@
 extends Spatial
+
+onready var player = $Player
