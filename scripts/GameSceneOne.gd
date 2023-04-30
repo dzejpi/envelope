77d2dec75a97e9899c60eaf31b3496c6015055a3
DzejPi <dzejjpii@gmail.com>
2023-04-30 12:04:40 +0200

    Moved the player into the starting position, created first dialog.

--- scripts/GameSceneOne.gd
+++ scripts/GameSceneOne.gd
@@ -9,6 +9,7 @@ var hideouts = []
 
 func _ready():
 	get_all_hideouts()
+	player.typewriter_dialog.start_dialog(["Fence closed on its own behind me. But I am sure someone will help me to open it.", "Seems that they are taking their security extra seriously here. Good for them."], get_process_delta_time())
 	
 	
 func get_all_hideouts():
@@ -17,3 +18,5 @@ func get_all_hideouts():
 		enemy.hideouts.append(node)
 	
 	enemy.find_closest_hideout(enemy.global_transform.origin)
+
+
